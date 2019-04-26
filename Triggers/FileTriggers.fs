a7a7d19d084a5bd25c862ffa1c51098e6f244b1b
Kenneth Jones <kendjone@iu.edu>
2019-04-26 15:17:10 -0500

    More boilerplate stuff

---
+++ Triggers/FileTriggers.fs
@@ -0,0 +1,9 @@
+namespace App
+
+// open Microsoft.Azure.WebJobs
+
+module FileTriggers =
+    // [<FunctionName("Log Change")>]
+    // let logChange ([<FileTrigger(@"dir/", "*log")>] file : FileStream ) =
+    //     ()
+    ()
---
+++ Triggers/TimerTriggers.fs
@@ -0,0 +1,11 @@
+namespace App
+
+// open Microsoft.Azure.WebJobs
+
+module TimerTriggers =
+    // [<FunctionName("Timer")>]
+    // let Timer ([<TimerTrigger("* * * * * *")>] timer:TimerInfo) =
+    //     let status = timer.ScheduleStatus 
+    //     let date = status.LastUpdated.ToLongDateString() 
+    //     System.Console.WriteLine(date)
+    ()
