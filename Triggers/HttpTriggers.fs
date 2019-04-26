67616655078b5a7fbd9b97ba5b1d13acbba44af7
Kenneth Jones <kendjone@iu.edu>
2019-04-26 15:28:53 -0500

    Remove unneeded attributes

--- Triggers/HttpTriggers.fs
+++ Triggers/HttpTriggers.fs
@@ -2,14 +2,10 @@ namespace App
 
 open Microsoft.Azure.WebJobs
 open System.Net.Http
-open Microsoft.Azure.WebJobs.Extensions.Http
 open Models
 open HttpHelpers
 
 module HttpTriggers =
     [<FunctionName("Ping")>]
-    let ping ([<HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "ping")>] req : HttpRequestMessage) =
-        let data =
-            { Name = "Pong!"
-              Description = "This route is used to test if the app is running" }
-        JsonOkResponse data
+    let ping ([<HttpTrigger(Route = "ping")>] req : HttpRequestMessage) =
+        JsonOkResponse { Name = "Pong!"; Description = "This route is used to test if the app is running" }
